335f9544894ec30a166636300f5c7b03296018ff
Devin Joubert <dvnjoubert0@gmail.com>
2024-12-11 11:06:50 +0200

    Add Day 1

---
+++ src/main/scala/Day1.scala
@@ -0,0 +1,23 @@
+import scala.io.Source
+import scala.util.Using
+
+object Day1 {
+  def main(args: Array[String]): Unit = {
+    val maybeLists =
+      Using(Source.fromResource("day1.txt")) { source =>
+        val lines = source.getLines().toList
+        lines
+          .map(_.split("\\s+").map(_.toInt) match { case Array(a, b) => (a, b) })
+          .unzip
+      }
+
+    val (left, right) = maybeLists.getOrElse(throw new Exception("Failed to read input"))
+
+    val distances = left.sorted.zip(right.sorted).map((a, b) => math.abs(a - b)).sum
+    println(s"Day 1 part 1 - $distances")
+
+    val rightCounts = right.groupBy(identity).view.mapValues(_.size)
+    val similarity = left.map(n => n * rightCounts.getOrElse(n, 0)).sum
+    println(s"Day 1 part 2 - $similarity")
+  }
+}
